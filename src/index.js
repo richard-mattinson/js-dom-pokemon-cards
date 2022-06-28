9ac412510d34a377375207703acf0dcedf8e3a05
Richard Mattinson <richard.m@ttinson.net>
2022-06-28 17:02:26 +0100

    extension 1 most working

--- src/index.js
+++ src/index.js
@@ -2,7 +2,33 @@
 //You can start simple and just render a single 
 //pokemon card from the first element
 
-// this querySelector gives access to <ul class "card"> in index.html
+// const header = document.createElement('header');
+// document.body.append(header);
+// header.setAttribute('header');
+
+// const h1El = document.querySelector('h1');
+// header.append(h1El);
+
+const h1El = document.querySelector('h1');
+h1El.addEventListener('click', function() {
+  h1El.style.color = randomColors()
+})
+
+const p = document.createElement('p');
+document.body.append(p);
+p.textContent = 'Here are all my Pokemon cards, let me know if you want to trade';
+p.addEventListener('click', function() { 
+  p.style.color = randomColors()
+})
+
+// Random Colour Generator
+const toHex = 16
+
+function randomColors() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(toHex);
+}
+
+// this querySelector gives access to <ul class "cards"> in index.html
 const cardsEl = document.querySelector('.cards');
 
 // loop through the entire data file. 
@@ -13,7 +39,10 @@ for (let index = 0; index < data.length; index++) {
   const card = document.createElement("li");
   cardsEl.append(card);
   card.setAttribute("class", "card");
-  card.style.listStyle = "none"
+  card.style.listStyle = "none";
+  card.addEventListener("click", function () {
+    card.style.background = randomColors();
+  });
 
   // Name
   const h2El = document.createElement("h2");
@@ -37,7 +66,7 @@ for (let index = 0; index < data.length; index++) {
   statsUl.ul;
   statsUl.style.listStyle = "none";
   statsUl.style.textTransform = "uppercase";
-  statsUl.style.lineHeight = "2"
+  statsUl.style.lineHeight = "2";
   statsUl.style.fontWeight = "540";
 
   //Stat li HP
@@ -69,6 +98,24 @@ for (let index = 0; index < data.length; index++) {
   const statSpeed = document.createElement("li");
   statsUl.appendChild(statSpeed);
   statSpeed.innerText = "Speed: " + pokemon.stats[4].base_stat;
+
+  // Versions
+  const appearancesList = document.createElement("div");
+  card.append(appearancesList)
+  appearancesList.setAttribute("class", "card--text");
+  appearancesList.innerText = "Appearances: "
+  appearancesList.style.fontWeight = "600";
+
+  for (let index = 0; index < pokemon.game_indices.length; index++) {
+  const allAppearences = pokemon.game_indices[index];
+
+  const appearances = document.createElement("p");
+  appearancesList.append(appearances);
+  appearances.style.textTransform = "capitalize";
+  appearances.style.display = "inline";
+  appearances.innerText = allAppearences.version.name + ", ";
+  appearances.style.fontWeight = "200";
+  }
 }
 
 // console.log(data[0]);
